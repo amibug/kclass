557801780ea3809e7a516631cbd91bf4d180d79c
amibug <hzwuyuedong@corp.netease.com>
2015-04-20 19:40:28 +0800

    demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,33 +1,41 @@
-var Person = Kclass.extend({
-    init: function(){
-        console.log('initPerson');
-    },
-    call: function(){
-        console.log('call');
-    }
-});
-var Son = Person.extend({
-    init: function(){
-        this.supr();
-        console.log('initSon');
-    }
-});
-var Daughter = Person.extend({
-    init: function(){
-        this.supr();
-        console.log('initDaughter');
-    }
-});
+(function() {
+
+    var Person = Kclass.extend({
+        init: function () {
+            console.log('initPerson');
+        },
+        call: function () {
+            console.log('call');
+        }
+    });
+    var Son = Person.extend({
+        supr: function (o) {
+            console.log('supr');
+        },
+        init: function () {
+            this.supr();
+            console.log('initSon');
+        }
+    });
+    var Daughter = Person.extend({
+        init: function () {
+            this.supr();
+            console.log('initDaughter');
+        }
+    });
 
 
-var Tom = new Son({
-    data: 'xxx'
-});
-var Lili = new Daughter();
+    var Tom = new Son({
+        data: 'xxx'
+    });
+    var Lili = new Daughter();
 
-Tom.on('init', function(){console.log('initSonCB');});
-Tom.emit('init');
-Lili.emit('init');
-debugger;
-Tom.off('init');
-Tom.emit('init');
+    Tom.on('init', function () {
+        console.log('initSonCB');
+    });
+    Tom.emit('init');
+    Lili.emit('init');
+    debugger;
+    Tom.off('init');
+    Tom.emit('init');
+}());
